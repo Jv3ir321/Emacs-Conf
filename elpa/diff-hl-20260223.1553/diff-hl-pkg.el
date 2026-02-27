28baf4cc91f4f44a09fca8aabc5522320b393a67
Jv3ir321 <jv3ir321@proton.me>
2026-02-26 21:17:01 -0500

    first commit

---
+++ elpa/diff-hl-20260223.1553/diff-hl-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "diff-hl" "20260223.1553"
+  "Highlight uncommitted changes using VC."
+  '((cl-lib "0.2")
+    (emacs  "26.1"))
+  :url "https://github.com/dgutov/diff-hl"
+  :commit "bdb36417e3dc990326567803831241199e266844"
+  :revdesc "bdb36417e3dc"
+  :keywords '("vc" "diff")
+  :authors '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainers '(("Dmitry Gutov" . "dmitry@gutov.dev")))
---
+++ elpa/kanagawa-themes-20260102.1542/kanagawa-themes-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "kanagawa-themes" "20260102.1542"
+  "Elegant theme inspired by The Great Wave off Kanagawa."
+  '((emacs "24.3"))
+  :url "https://github.com/Fabiokleis/kanagawa-emacs"
+  :commit "95516102a673b924c0446ae548c9053773f878ea"
+  :revdesc "95516102a673"
+  :keywords '("themes" "faces")
+  :authors '(("Sion Eltnam Sokaris" . "meritamen@sdf.org"))
+  :maintainers '(("Fabio Kleis" . "fabiohkrc@gmail.com")
+                 ("Sion Eltnam Sokaris" . "meritamen@sdf.org")))
