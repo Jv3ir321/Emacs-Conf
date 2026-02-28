8091963f5f030376bf2b55983938bb2efe111ec9
Jv3ir321 <jv3ir321@proton.me>
2026-02-27 21:56:14 -0500

    s

--- init.el
+++ init.el
@@ -48,6 +48,17 @@
 
 (setq use-package-always-ensure t)
 (add-to-list 'auto-mode-alist '("xmonad\\.hs\\'" . haskell-mode))
+
+;; Rust moderno con tree-sitter
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
+
+(use-package lsp-mode
+  :hook (rust-ts-mode . lsp)
+  :commands lsp)
+
+(use-package lsp-ui
+  :commands lsp-ui-mode)
+
 ;; =========================
 ;; THEME KANAGAWA
 ;; =========================
@@ -55,7 +66,38 @@
 (use-package kanagawa-themes
   :config
   (load-theme 'kanagawa-wave t))
+;; =========================
+;; TAB-LINE ESTILO KANAGAWA
+;; =========================
 
+(global-tab-line-mode 1)
+
+(setq tab-line-close-button-show nil)
+(setq tab-line-new-button-show nil)
+(setq tab-line-separator "  ")
+
+;; colores estilo kanagawa-wave
+(custom-set-faces
+ '(tab-line
+   ((t (:background "#1f1f28"
+        :foreground "#727169"
+        :box nil))))
+
+ '(tab-line-tab
+   ((t (:background "#2a2a37"
+        :foreground "#c8c093"
+        :box (:line-width 6 :color "#2a2a37")))))
+
+ '(tab-line-tab-current
+   ((t (:background "#363646"
+        :foreground "#e6c384"
+        :box (:line-width 6 :color "#363646")
+        :weight bold))))
+
+ '(tab-line-tab-inactive
+   ((t (:background "#1f1f28"
+        :foreground "#727169"
+        :box (:line-width 6 :color "#1f1f28"))))))
 ;; =========================
 ;; EVIL MODE
 ;; =========================
@@ -68,6 +110,26 @@
   (evil-mode 1))
 
 ;; =========================
+;; TOGGLE TAB-LINE
+;; =========================
+
+(defun my/toggle-tabs ()
+  (interactive)
+  (if global-tab-line-mode
+      (progn
+        (global-tab-line-mode -1)
+        (message "Tabs OFF"))
+    (global-tab-line-mode 1)
+    (message "Tabs ON")))
+
+;; Keybind global
+(global-set-key (kbd "C-c t") #'my/toggle-tabs)
+
+;; Keybind en modo normal (Evil)
+(with-eval-after-load 'evil
+  (evil-define-key 'normal 'global
+    (kbd "<leader>tt") #'my/toggle-tabs))
+;; =========================
 ;; COMPLETADO MODERNO
 ;; =========================
 
@@ -75,6 +137,9 @@
   :config
   (global-company-mode 1))
 
+(add-hook 'pdf-view-mode-hook
+          (lambda ()
+            (display-line-numbers-mode -1)))
 ;; =========================
 ;; LSP PARA PROGRAMAR
 ;; =========================
@@ -90,7 +155,13 @@
 ;; ORG MODE PARA NOTAS
 ;; =========================
 
+(require 'org-habit)
+(add-to-list 'org-modules 'org-habit)
+(setq org-habit-graph-column 60) ; dónde aparece el gráfico
+
 (setq org-directory "~/org")
+
+(setq org-agenda-files '("~/org/agenda/"))
 
 (setq org-default-notes-file
       (concat org-directory "/notes.org"))
@@ -99,7 +170,8 @@
 
 (setq org-hide-emphasis-markers t)
 
-;; =========================
+(setq org-log-into-drawer t)
+; =========================
 ;; MEJORAS GENERALES
 ;; =========================
 
@@ -145,13 +217,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(company consult dashboard diff-hl doom-modeline evil-collection
-             evil-nerd-commenter go-mode haskell-mode kanagawa-themes
-             lsp-ui lua-mode marginalia orderless org-bullets
-             projectile python-mode rust-mode tree-sitter-langs
-             treemacs vertico vterm vterm-hotkey vterm-toggle web-mode
-             with-editor)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
